a2877ee700a8a52a92d9f860fe1cd9265989e8b2
Popfan999552 <yimsoq@gmail.com>
2023-10-18 19:45:32 -0700

    ewf
    wef

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/test/TestServer.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.test;
+//package com.acmerobotics.dashboard;
+
+public class TestServer {
+    public static void main(String[] args) throws InterruptedException {
+        TestDashboardInstance.getInstance().start();
+    }
+}
